7ba63361f8bdebc4e67cea35672a7031ca9f4761
Pham Hong Duc <iamphduc@gmail.com>
2021-08-30 19:10:55 +0700

    add new url

--- background.js
+++ background.js
@@ -1,15 +1,26 @@
 const loginURL = "https://stdportal.tdtu.edu.vn/Login/Index";
 
 const SCHOOL_URL = {
+  // basic-theme
   home: "https://stdportal.tdtu.edu.vn/Home/Index",
   notification: "https://studentnews.tdtu.edu.vn/Home/Index",
   schedule: "https://lichhoc-lichthi.tdtu.edu.vn/tkb2.aspx",
   score: "https://ketquahoctap.tdtu.edu.vn/home/diemhocky",
   train: "https://stdportal.tdtu.edu.vn/main/hoatdongphongtrao",
-  elearning: "https://stdportal.tdtu.edu.vn/main/elearningv2",
+  "new-elearning": "https://stdportal.tdtu.edu.vn/main/elearningv2",
   exam: "https://lichhoc-lichthi.tdtu.edu.vn/xemlichthi.aspx",
   map: "https://learninginfo.tdtu.edu.vn/sv_xemctdt",
   tuition: "https://hocphilephi.tdtu.edu.vn/",
+  // old-portal-theme
+  elearning: "https://stdportal.tdtu.edu.vn/main/elearning",
+  survey: "https://survey-beta.tdtu.edu.vn/SinhVien",
+  info: "https://stdportal.tdtu.edu.vn/main/thongtinsinhvien",
+  registration: "http://dkmh.tdt.edu.vn/",
+  "elearning-skill": "https://elearning-ability.tdtu.edu.vn/Home",
+  "student-cert": "https://cnsv.tdtu.edu.vn/",
+  "apply-online": "http://nopdon.tdtu.edu.vn/",
+  rule: "https://quychehocvu.tdtu.edu.vn/QuyChe/Index",
+  "sport-club": "http://sport.tdtu.edu.vn/",
 };
 
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
